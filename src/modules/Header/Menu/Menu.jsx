b9d050cff5ca382d602fd4637eaf566061dca1bc
Camilo Polania <cpolania@outlook.com>
2023-01-18 19:19:08 -0500

    Menu mobile en proceso

--- src/modules/Header/Menu/Menu.jsx
+++ src/modules/Header/Menu/Menu.jsx
@@ -1,9 +1,13 @@
 // Imports
+import { SwipeableDrawer } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import IconMenu from "../../../assets/svg/icon-menu";
 // Módulo de gestión del menu de inicio
 const Menu = () => {
     // Lista del menu
+    const [open, setOpen] = useState(false)
     const menuItems = [
         {
             id: 'MENU_01',
@@ -33,15 +37,44 @@ const Menu = () => {
     ]
     // Return del componente
     return (
-        <menu>
-            {
-                menuItems.map(item => (
-                    <Link to={item.to} key={item.id}>
-                        <h2>{item.title}</h2>
-                    </Link>
-                ))
-            }
-        </menu>
+        <div className="container-menu">
+            <menu className="menu-desktop">
+                {
+                    menuItems.map(item => (
+                        <Link to={item.to} key={item.id}>
+                            <h2>{item.title}</h2>
+                        </Link>
+                    ))
+                }
+            </menu>
+            <>
+                <button
+                    onClick={() => setOpen(true)}
+                    className="btn-menu-mobile">
+                    <IconMenu />
+                </button>
+                <SwipeableDrawer
+                    open={open}
+                    onOpen={() => setOpen(true)}
+                    onClose={() => setOpen(false)}
+                    ModalProps={{
+                        keepMounted: false
+                    }}
+                >
+                    <div className="menu-container">
+                        {menuItems.map(item => (
+                            <Link
+                                to={item.to}
+                                key={item.id}
+                                onClick={() => setOpen(false)}>
+                                {item.title}
+                            </Link>
+                        ))}
+
+                    </div>
+                </SwipeableDrawer>
+            </>
+        </div>
     )
 }
 export default Menu;
--- src/modules/Header/header.jsx
+++ src/modules/Header/header.jsx
@@ -30,9 +30,7 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className="container-menu">
-                <Menu />
-            </div> */}
+            <Menu />
         </header>
     )
 }
